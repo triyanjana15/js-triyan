3d1e8c63a1aa94fb2d9b2069361bab8d58a10106
Triyanjana Paul <112961612+triyanjana15@users.noreply.github.com>
2024-01-15 15:32:13 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "triyan@google.com"
+var accountPassword ="12345"
+accountCity = "kolkata"
+let accountState;
+// accountId = 2// not allowed
+accountEmail = "hc@hc.com"
+accountPassword =" 212121"
+accountCity = "nabadwip"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
