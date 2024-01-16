f68c46462f000833bc3568d7b2d75195bb45654d
Triyanjana Paul <112961612+triyanjana15@users.noreply.github.com>
2024-01-16 13:02:39 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+ "use strict"; //treat all js code as newer version
+
+//  alert(3+3) // we are using nodejs,not browser
+
+console.log(3+3); console.log("hitesh") //code readability should be high
+
+let name="hitesh" //string type
+let age =20 //number type
+let isLoggedIn=false //boolean type
+
+//number => 2 to power 53
+// bigInt for very big number 
+// string => ""
+//boolean => true / false
+//null is a standalone value
+//undefined => when value is not yet assigned
+//symbol=> unique 
+
+// object
+
+console.log(typeof undefined);//type is undefined
+console.log(typeof null);//type is object
