896ba1ba780b87f596db5beea1d497359fd4633a
Triyanjana Paul <112961612+triyanjana15@users.noreply.github.com>
2024-01-16 15:24:32 +0000

    operations

--- 01_basics/03_conversionsoroperations.js
+++ 01_basics/03_conversionsoroperations.js
@@ -19,3 +19,25 @@ console.log(booleanIsLoggedIn);
 //"string"=>true
 //number=>true
 
+//*********operations********//
+
+let value = 3 
+let negvalue = -value
+console.log(negvalue);
+
+let str1="hello"
+let str2="hitesh"
+let str3 = str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
